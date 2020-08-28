d932cd175bf9c4a3eb4f4bdc86c63043671f578b
Mikhail D'Souza <mdsouza@atlassian.com>
2020-08-28 11:56:00 +1000

    Producing a phat jar for cross-platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,17 @@
+val javaFxVersion = "11.0.2"
+
 plugins {
     java
     id("org.springframework.boot") version "2.3.3.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    id("org.openjfx.javafxplugin") version "0.0.8"
+    id("org.openjfx.javafxplugin") version "0.0.9"
 
     kotlin("jvm") version "1.4.0"
     kotlin("plugin.spring") version "1.4.0"
 }
-
 group = "io.atlassian"
 version = "0.0.1-SNAPSHOT"
+
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -18,7 +20,6 @@ repositories {
 }
 
 extra["springCloudVersion"] = "Hoxton.SR7"
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
 
@@ -30,6 +31,12 @@ dependencies {
 
     implementation("no.tornado:tornadofx:1.7.20")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.3.9")
+
+    listOf("mac", "linux", "win").forEach {
+        runtimeOnly("org.openjfx:javafx-graphics:$javaFxVersion:$it")
+        runtimeOnly("org.openjfx:javafx-base:$javaFxVersion:$it")
+        runtimeOnly("org.openjfx:javafx-controls:$javaFxVersion:$it")
+    }
 
     implementation("com.amazonaws:aws-java-sdk-cloudwatch:1.11.842")
 
@@ -45,7 +52,7 @@ dependencyManagement {
 }
 
 javafx {
-    version = "11.0.2"
+    version = javaFxVersion
     modules = listOf("javafx.controls", "javafx.graphics")
 }
 
